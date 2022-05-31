e4630ac70524e30845815087c2d6275f3a3992e4
Lukasxs93 <lucaporcelli93@gmail.com>
2022-05-31 16:53:30 +0100

    random positioning of answers and solutions

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 
 
-fetch('https://opentdb.com/api.php?amount=10')
+fetch('https://opentdb.com/api.php?amount=10&category=21&type=multiple')
 
 .then(response =>  response.json() )
 
 .then(data =>  {
     for (let i = 0; i < data.results.length; i++) {
+        
+        function shuffle(answers){
+            let currentIndex = answers.length;
+            let randomIndex;
+            while(currentIndex != 0 ){
+              randomIndex = Math.floor(Math.random() * currentIndex);
+              currentIndex --;
+              [answers[currentIndex], answers[randomIndex]]=[ answers[randomIndex], answers[currentIndex]];
+            }
+            return answers;
+        }   
+        let wrongAnswers = data.results[i].incorrect_answers;
+        let rightAnswer = data.results[i].correct_answer;
+         wrongAnswers.push(rightAnswer);
+        let solutions = [];
+        solutions.push(rightAnswer);
+        shuffle(wrongAnswers);
+
+        
         let ques = data.results[i].question;
+        console.log(solutions);
+      
         let questionItem = document.createElement('div');
-        questionItem.innerHTML =`<h2>${ques}</h2>`;
-        document.body.appendChild(questionItem);
+       
+        questionItem.className='question';
+        
+        questionItem.innerHTML =`
+        
+        <h2>${ques}</h2><br>
+        <ul>
+          <li>${wrongAnswers[0]}</li>
+          <li>${wrongAnswers[1]}</li>
+          <li>${wrongAnswers[2]}</li>
+          <li>${wrongAnswers[3]}</li>
+        </ul>
+        `;
+        document.body.appendChild(questionItem);       
+        
        
     }  
+
 
 } )
 .then( )
