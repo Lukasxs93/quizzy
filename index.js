84835325585b34dec8e5cd96008358592a47aee8
Lukasxs93 <lucaporcelli93@gmail.com>
2022-06-01 01:55:08 +0100

    answer feedback

--- index.js
+++ index.js
@@ -6,8 +6,18 @@ fetch('https://opentdb.com/api.php?amount=10&category=21&type=multiple')
 
 .then(data =>  {
     for (let i = 0; i < data.results.length; i++) {
+      function makeid(length) {
+        var result           = '';
+        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+          result += characters.charAt(Math.floor(Math.random() * 
+     charactersLength));
+       }
+       return result;
+    }
         
-        function shuffle(answers){
+    function shuffle(answers){
             let currentIndex = answers.length;
             let randomIndex;
             while(currentIndex != 0 ){
@@ -16,7 +26,17 @@ fetch('https://opentdb.com/api.php?amount=10&category=21&type=multiple')
               [answers[currentIndex], answers[randomIndex]]=[ answers[randomIndex], answers[currentIndex]];
             }
             return answers;
-        }   
+    }  
+
+    function validate(idValue){
+      let elem = document.getElementById(idValue).innerHTML;
+       if(solutions.includes(elem)){
+          document.getElementById(idValue).className += ' correct';
+       }else{
+        document.getElementById(idValue).className +=' incorrect';
+       }
+    }
+         
         let wrongAnswers = data.results[i].incorrect_answers;
         let rightAnswer = data.results[i].correct_answer;
          wrongAnswers.push(rightAnswer);
@@ -26,30 +46,40 @@ fetch('https://opentdb.com/api.php?amount=10&category=21&type=multiple')
 
         
         let ques = data.results[i].question;
+        
         console.log(solutions);
+        console.log(data);
       
         let questionItem = document.createElement('div');
        
         questionItem.className='question';
+
+        var idArray = [makeid(5),makeid(5),makeid(5),makeid(5)]
+        
         
         questionItem.innerHTML =`
         
         <h2>${ques}</h2><br>
-        <ul>
-          <li>${wrongAnswers[0]}</li>
-          <li>${wrongAnswers[1]}</li>
-          <li>${wrongAnswers[2]}</li>
-          <li>${wrongAnswers[3]}</li>
-        </ul>
+        <div class="answer_container">
+          <button class="answer " id="${idArray[0]}">${wrongAnswers[0]}</button>
+          <button class="answer " id="${idArray[1]}">${wrongAnswers[1]}</button>
+          <button class="answer " id="${idArray[2]}">${wrongAnswers[2]}</button>
+          <button class="answer " id="${idArray[3]}">${wrongAnswers[3]}</button>
+        </div>
         `;
-        document.body.appendChild(questionItem);       
         
+        
+        document.body.appendChild(questionItem);       
+        document.getElementById(idArray[0]).addEventListener('click',validate.bind(this,idArray[0]),false);
+        document.getElementById(idArray[1]).addEventListener('click',validate.bind(this,idArray[1]),false);
+        document.getElementById(idArray[2]).addEventListener('click',validate.bind(this,idArray[2]),false);
+        document.getElementById(idArray[3]).addEventListener('click',validate.bind(this,idArray[3]),false);
        
     }  
-
+    
 
 } )
-.then( )
+
 
 
 fetch('https://opentdb.com/api.php?amount=10')
