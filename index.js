98484ba1cb6e4ed3d93d3c8ed92b98ea309c1d03
Lukasxs93 <lucaporcelli93@gmail.com>
2022-06-03 23:50:41 +0100

    minor changes

--- index.js
+++ index.js
@@ -1,58 +1,75 @@
-
+let solutions = [];
+let score=0;
 // getting the data from opentdb.com
 fetch('https://opentdb.com/api.php?amount=10&category=21&type=multiple')
 // unpacking the jason file 
 .then(response =>  response.json() )
 
 .then(data =>  { 
+   // makeid generates a unique id to be added to the signle element in the object
+   function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
+}
+
+// shuffle rearanges the order of the answers so they are less predictable   
+function shuffle(answers){
+        let currentIndex = answers.length;
+        let randomIndex;
+        while(currentIndex != 0 ){
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex --;
+          [answers[currentIndex], answers[randomIndex]]=[ answers[randomIndex], answers[currentIndex]];
+        }
+        return answers;
+}  
+// validate checks if the answer is correct or incorrect and injects a class to the button to style and produce feedback
+function validate(idValue){
+  let elem = document.getElementById(idValue).innerHTML;
+   if(solutions.includes(elem)){
+      document.getElementById(idValue).classList.toggle('correct');
+      score ++ ;
+      let scoreItem = document.createElement('div');
+     if(document.getElementById('score') !== null){
+      
+     };
+     // 
+    scoreItem.innerHTML=`<h1 id="score">${score}</h1>`;
+    document.body.appendChild(scoreItem);
+      
+   }else{
+    document.getElementById(idValue).classList.toggle('incorrect');
+   }
+}
+// function getScore(){
+  // if(document.getElementById('score')){
+// 
+  // }
+// }
+
+
   // for each object in the data we perform an action 
     for (let i = 0; i < data.results.length; i++) {
-  // makeid generates a unique id to be added to the signle element in the object
-      function makeid(length) {
-        var result           = '';
-        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        var charactersLength = characters.length;
-        for ( var i = 0; i < length; i++ ) {
-          result += characters.charAt(Math.floor(Math.random() * 
-     charactersLength));
-       }
-       return result;
-    }
-  // shuffle rearanges the order of the answers so they are less predictable   
-    function shuffle(answers){
-            let currentIndex = answers.length;
-            let randomIndex;
-            while(currentIndex != 0 ){
-              randomIndex = Math.floor(Math.random() * currentIndex);
-              currentIndex --;
-              [answers[currentIndex], answers[randomIndex]]=[ answers[randomIndex], answers[currentIndex]];
-            }
-            return answers;
-    }  
-  // validate checks if the answer is correct or incorrect and injects a class to the button to style and produce feedback
-    function validate(idValue){
-      let elem = document.getElementById(idValue).innerHTML;
-       if(solutions.includes(elem)){
-          document.getElementById(idValue).classList.toggle('correct');
-       }else{
-        document.getElementById(idValue).classList.toggle('incorrect');
-       }
-    }
-         
-        let wrongAnswers = data.results[i].incorrect_answers;
-        let rightAnswer = data.results[i].correct_answer;
+ 
+      let wrongAnswers = data.results[i].incorrect_answers;
+      let rightAnswer = data.results[i].correct_answer;
+        
         // creating an array of  all answers (no matter if correct or incorrect )
          wrongAnswers.push(rightAnswer);
         // creating an array of all the right answers for validation purposes
-        let solutions = [];
-        solutions.push(rightAnswer);
+        solutions.push(rightAnswer)
         shuffle(wrongAnswers);
-
+        
         
         let ques = data.results[i].question;
         
-        console.log(solutions);
-        console.log(data);
+       
       
         let questionItem = document.createElement('div');
        
@@ -82,6 +99,7 @@ fetch('https://opentdb.com/api.php?amount=10&category=21&type=multiple')
         document.getElementById(idArray[3]).addEventListener('click',validate.bind(this,idArray[3]),false);
        
     }  
+  
     
 
 } )
